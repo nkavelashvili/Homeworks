6eb9252fd16d22c9d45fd571bda8c8dee9db87bb
nkavelashvili <85543832+nkavelashvili@users.noreply.github.com>
2021-12-27 13:40:56 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("shemoitane sheni saxeli da gvari");
+            Console.WriteLine(Console.ReadLine());
+            Console.BackgroundColor = ConsoleColor.Blue;
+        }
+    }
+}
